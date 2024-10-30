852664fc3eacfb11fd27d1329f2ff0be0d93131c
HadyElMaageny <87091270+HadyElMaageny@users.noreply.github.com>
2024-10-30 04:28:38 +0300

    Header, Footer, Navbar, Sidebar compoents and Store Class

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Istore } from '../../models/istore';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  myStore: Istore;
+
+  constructor() {
+    this.myStore = {
+      name: "H&M",
+      imgUrl: "https://fakeimg.pl/300/",
+      branches: ["Cairo", "Alex", "Canada"]
+    }
+  }
+}
